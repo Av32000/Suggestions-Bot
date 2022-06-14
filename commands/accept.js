3b8baeec9f175b6f7dc6dcf900c4d76bfb2afbf3
Av32000 <ae.av32000@gmail.com>
2022-06-15 00:58:39 +0200

    Patch Crash if fake msg

--- commands/accept.js
+++ commands/accept.js
@@ -64,7 +64,7 @@ class Command {
 
         }
 
-      });
+      }).catch(console.error);
     }
   }
 }
--- commands/comment.js
+++ commands/comment.js
@@ -55,7 +55,7 @@ class Command {
 
         }
 
-      });
+      }).catch(console.error);
     }
   }
 }
